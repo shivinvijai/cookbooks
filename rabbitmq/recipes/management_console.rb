53941cd0534a874d1d05dd2af130f9b61ffe3946
Shivin Vijai <shivinvijai@gmail.com>
2018-01-15 13:54:52 +0530

    Issue fix.

--- rabbitmq/recipes/management_console.rb
+++ rabbitmq/recipes/management_console.rb
@@ -2,8 +2,6 @@ include_recipe 'rabbitmq::default'
 
 plugins = %w( rabbitmq_management rabbitmq_management_visualiser )
 
-service_name = node['rabbitmq']['service_name']
-
 plugins.each do |plugin|
   rabbitmq_plugin plugin do
     action :enable
