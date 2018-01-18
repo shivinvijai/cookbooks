a1d86686db6f29057034754072c57e5818487de4
Shivin Vijai <shivinvijai@gmail.com>
2018-01-18 22:15:20 +0530

    Update default.rb

--- rabbitmq-bash-cookbook/recipes/default.rb
+++ rabbitmq-bash-cookbook/recipes/default.rb
@@ -6,17 +6,30 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-bash 'install_rabbitmq' do
+bash 'install_rabbitmq_key' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
   wget --quiet -O - https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-  sudo dpkg -i erlang-solutions_1.0_all.deb
+   sudo dpkg -i erlang-solutions_1.0_all.deb
   sudo apt-get update -y
   sudo apt-get install erlang erlang-nox -y
   echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
   wget --quiet -O - https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-  sudo apt-get update -y
+  udo apt-get update -y
+  EOH
+end
+bash 'install_rabbitmq' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+#  wget --quiet -O - https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+#  sudo dpkg -i erlang-solutions_1.0_all.deb
+ # sudo apt-get update -y
+  #sudo apt-get install erlang erlang-nox -y
+  #echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+#  wget --quiet -O - https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+#  sudo apt-get update -y
   sudo apt-get install rabbitmq-server -y
   sudo update-rc.d rabbitmq-server defaults
   sudo service rabbitmq-server start
