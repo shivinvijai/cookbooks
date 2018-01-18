586103161340048956edc0e32a8530f8900a724d
Shivin Vijai <shivinvijai@gmail.com>
2018-01-18 23:00:32 +0530

    Update default.rb

--- rabbitmq-bash-cookbook/recipes/default.rb
+++ rabbitmq-bash-cookbook/recipes/default.rb
@@ -6,32 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-bash 'install_rabbitmq_key' do
+bash 'install_rabbitmq' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
   wget --quiet -O - https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+  sudo dpkg -i erlang-solutions_1.0_all.deb
+  sudo apt-get update -y
+  sudo apt-get install erlang erlang-nox -y
   echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
   wget --quiet -O - https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
   sudo apt-get update -y
-  EOH
-end
-
-package 'rabbitmq-server-install' do
-  package_name 'rabbitmq-server'
-  package_name 'erlang'
-  package_name 'erlang-nox'
-end
-
-bash 'create_users' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
+  sudo apt-get install rabbitmq-server -y
   sudo update-rc.d rabbitmq-server defaults
+  sudo service rabbitmq-server start
   sudo rabbitmqctl add_user admin password
   sudo rabbitmqctl set_user_tags admin administrator
   sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
   sudo rabbitmq-plugins enable rabbitmq_management
   EOH
 end
-
