3edf8981c6f7915fe16056be2e266bc51d957618
Shivin Vijai <shivinvijai@gmail.com>
2018-01-26 22:37:57 +0530

    LAMP Install.

---
+++ lamp_install/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: LAMP Installation
+# Recipe:: default
+#
+# Copyright 2018, Shivin Vijai
+#
+# All rights reserved.
+#
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  supports status: true, restart: true, reload: true
+  action [ :enable, :start ]
+end
+
+cookbook_file '/usr/share/nginx/html/index.html' do
+  source 'index.html'
+  action :create
+end
+
+# Install the mod_php5 Apache module.
+package 'php5' do
+  action :install
+  notifies :restart, 'nginx', :immediately
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+end
+
+package 'mysql-server' do
+  action :install
+end
+
+template "/etc/my.cnf" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "my.conf.erb"
+end
+
+service "mysql" do
+  supports :status => true, :restart => true, :reload => true
+  action :enable
+end
