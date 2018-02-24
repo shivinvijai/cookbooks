85b9ea87263f41a6e2b8ea79083024ed859c1740
Shivin Vijai <shivinvijai@gmail.com>
2018-02-24 11:25:21 +0530

    Cookbook for Java8

---
+++ java8/metadata.rb
@@ -0,0 +1,9 @@
+name             'java8'
+maintainer       'Shivin Vijai'
+maintainer_email 'shivinvijai@gmail.com'
+license          'all_rights'
+description      'Installs/Configures java8'
+long_description 'Installs/Configures java8'
+version          '0.1.0'
+
+depends 'java'
